85bb43945b7c4165ada79016d7b4c690531b7421
MinnyNatchaya <mintnumber2@gmail.com>
2021-10-22 00:19:35 +0700

    finish profile edit

--- src/components/layout/profile/ProfileEditContainer.js
+++ src/components/layout/profile/ProfileEditContainer.js
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
-import { Box, margin, padding, styled } from "@mui/system";
-import { Button, TextField } from "@mui/material";
-import SideBarProfileL from "../sidebar/SideBarProfileL";
-import ProfileEditForm from "./ProfileEditForm";
+import React, { useState } from 'react';
+import { Box, margin, padding, styled } from '@mui/system';
+import { Button, TextField } from '@mui/material';
+import SideBarProfileL from '../sidebar/SideBarProfileL';
+import ProfileEditForm from './ProfileEditForm';
 
 function ProfileEditContainer() {
-	return (
-		<Box
-			// className="BOXXXXXXXXX"
-			sx={{
-				width: "100%",
-				display: "flex",
-				alignItems: "center",
-			}}
-		>
-			<SideBarProfileL />
-			<ProfileEditForm />
-		</Box>
-	);
+  return (
+    <Box
+      // className="BOXXXXXXXXX"
+      sx={{
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#EFF1F3'
+      }}
+    >
+      <SideBarProfileL />
+      <ProfileEditForm />
+    </Box>
+  );
 }
 
 export default ProfileEditContainer;
