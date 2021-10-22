c268cbe5cf7bd3e7d69c75ee6d2148f6255bde50
samatchacc9 <samatcha.matcha@gmail.com>
2021-10-22 15:09:45 +0700

    Create Home

---
+++ src/components/card/MainComment.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+import { Paper } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import { purple } from '@mui/material/colors';
+import CreateComment from './CreateComment';
+
+function MainComment() {
+  return (
+    <Stack justifyContent='space-between' direction='row' spacing={1} mb='10px'>
+      <Avatar
+        sx={{ bgcolor: purple[500] }}
+        aria-label='recipe'
+        src='https://images.unsplash.com/photo-1494354145959-25cb82edf23d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80'
+        sx={{ width: 40, height: 40 }}>
+        C
+      </Avatar>
+
+      <Paper sx={{ padding: '10px' }}>
+        <Typography sx={{ display: 'inline' }} variant='body2' color='text.secondary'>
+          {' '}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque possimus perferendis ea aut molestias
+          accusantium eveniet tempore!
+        </Typography>
+        <Grid container spacing={5} mb='5px'>
+          <Grid item>
+            <ThumbUpOutlinedIcon />
+            <Typography sx={{ display: 'inline' }} variant='body2' color='text.secondary'>
+              5
+            </Typography>
+          </Grid>
+
+          <Grid item>
+            <ChatBubbleOutlineOutlinedIcon />
+            <Typography sx={{ display: 'inline' }} variant='body2' color='text.secondary'>
+              2
+            </Typography>
+          </Grid>
+        </Grid>
+        <CreateComment />
+      </Paper>
+    </Stack>
+  );
+}
+
+export default MainComment;
---
+++ src/components/card/Post.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import { Box } from '@mui/system';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import Avatar from '@mui/material/Avatar';
+import MainComment from './MainComment';
+import SubComment from './SubComment';
+import CreatComment from './CreateComment';
+
+function PostImg({ type, link }) {
+  return (
+    <Card sx={{ height: '100%', padding: '10px', marginBottom: '20px' }}>
+      {/* CardHeader */}
+      <CardHeader
+        avatar={
+          <Avatar
+            aria-label='recipe'
+            src='https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80'
+            sx={{ width: 40, height: 40 }}></Avatar>
+        }
+        title='Shrimp and Chorizo Paella'
+      />
+
+      {/* Card content*/}
+      <CardContent>
+        <Typography variant='body2' color='text.secondary'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi placeat quaerat quibusdam facilis quisquam
+          aliquam nulla veritatis fuga ea nihil.
+        </Typography>
+      </CardContent>
+
+      {/* Card media*/}
+      <Box sx={{ width: '100%', margin: 'auto', height: '300px', backgroundColor: '#EFF1F3' }}>
+        <CardMedia
+          sx={{ objectFit: 'contain' }}
+          // component='img'
+          component={type}
+          height='100%'
+          // image='https://images.unsplash.com/photo-1547891654-e66ed7ebb968?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80'
+          image={link}
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        />
+      </Box>
+
+      <Grid container sx={{ display: 'flex', justifyContent: 'space-evenly', mt: '10px', p: '16px' }}>
+        <Grid item>
+          <ThumbUpOutlinedIcon />
+          <Typography sx={{ display: 'inline' }} variant='body2' color='text.secondary'>
+            2
+          </Typography>
+        </Grid>
+        <Grid item>
+          <ChatBubbleOutlineOutlinedIcon />
+          <Typography sx={{ display: 'inline' }} variant='body2' color='text.secondary'>
+            5
+          </Typography>
+        </Grid>
+        <Grid item>
+          <ShareOutlinedIcon />
+          <Typography sx={{ display: 'inline' }} variant='body2' color='text.secondary'></Typography>
+        </Grid>
+      </Grid>
+
+      <CreatComment />
+
+      <MainComment />
+      <SubComment />
+    </Card>
+  );
+}
+
+export default PostImg;
