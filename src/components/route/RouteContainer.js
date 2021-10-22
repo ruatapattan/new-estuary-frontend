12d0e9876381d5192941da36a246afcb03d0c682
samatchacc9 <samatcha.matcha@gmail.com>
2021-10-22 17:32:32 +0700

    edit tag CardActions CreateComment.js

--- src/components/route/RouteContainer.js
+++ src/components/route/RouteContainer.js
@@ -1,9 +1,9 @@
-import { display } from "@mui/system";
-import { useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import { allPages } from "../../config/routes";
-import Footer from "../layout/footer/Footer";
-import Header from "../layout/header/Header";
+import { display } from '@mui/system';
+import { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { allPages } from '../../config/routes';
+import Footer from '../layout/footer/Footer';
+import Header from '../layout/header/Header';
 
 function RouteContainer() {
   const [noHeader, setNoHeader] = useState(false);
@@ -15,23 +15,15 @@ function RouteContainer() {
           style={{
             flexGrow: 1,
             // border: "10px solid red",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100%",
-            height: "100%",
-          }}
-        >
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: '100%',
+            height: '100%',
+          }}>
           {allPages.map((item, idx) => {
-            return (
-              <Route
-                key={idx}
-                exact
-                path={item.path}
-                component={item.component}
-              />
-            );
+            return <Route key={idx} exact path={item.path} component={item.component} />;
           })}
         </div>
       </Switch>
