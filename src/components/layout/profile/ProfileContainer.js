b8df9a96a003b3c33427995f148992622edd459c
nattapat.aur <nattapat.aur@gmail.com>
2021-10-24 13:53:19 +0700

    after-meeting

--- src/components/layout/profile/ProfileContainer.js
+++ src/components/layout/profile/ProfileContainer.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { Box } from '@mui/system';
-import SideBarProfileL from '../sidebar/SideBarProfileL';
-import ProfileForm from '../profile/ProfileForm';
-import ProfileEditForm from '../profile/ProfileEditForm';
-import CreactProductContainer from '../product/CreactProductContainer';
-import Banner from '../profile/Banner';
+import React from "react";
+import { Box } from "@mui/system";
+import SideBarProfileL from "../sidebar/SideBarProfileL";
+import ProfileForm from "../profile/ProfileForm";
+import ProfileEditForm from "../profile/ProfileEditForm";
+import CreactProductContainer from "../product/CreactProductContainer";
+import Banner from "../profile/Banner";
 
 function ProfileContainer() {
-  return (
-    <Box
-      flexWrap="wrap"
-      sx={{
-        width: '100%',
-        display: 'flex',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        backgroundColor: '#EFF1F3'
-      }}
-    >
-      <Banner />
+	return (
+		<Box
+			flexWrap="wrap"
+			sx={{
+				width: "100%",
+				display: "flex",
+				// justifyContent: 'center',
+				// alignItems: 'center',
+				backgroundColor: "#EFF1F3",
+			}}
+		>
+			<Banner />
 
-      <SideBarProfileL />
-      <ProfileForm />
-      {/* <ProfileEditForm /> */}
-      {/* <CreactProductContainer /> */}
-    </Box>
-  );
+			<SideBarProfileL />
+			<ProfileForm />
+			{/* <ProfileEditForm /> */}
+			{/* <CreactProductContainer /> */}
+		</Box>
+	);
 }
 
 export default ProfileContainer;
