76b6b9fbd68e2de6634a5c0055fa8bc4dcd67283
MinnyNatchaya <mintnumber2@gmail.com>
2021-11-04 10:59:40 +0700

    edit product

--- src/components/layout/product/EditProductForm.js
+++ src/components/layout/product/EditProductForm.js
@@ -1,35 +1,35 @@
-import React, { useEffect, useState } from "react";
-import axios from "../../../config/axios";
-import MenuItem from "@mui/material/MenuItem";
-import { useHistory, useLocation } from "react-router-dom";
-import { Box } from "@mui/system";
-import { Button, Input, TextField } from "@mui/material";
-import Swal from "sweetalert2";
+import React, { useEffect, useState } from 'react';
+import axios from '../../../config/axios';
+import MenuItem from '@mui/material/MenuItem';
+import { useHistory, useLocation } from 'react-router-dom';
+import { Box } from '@mui/system';
+import { Button, Input, TextField } from '@mui/material';
+import Swal from 'sweetalert2';
 
 function EditProductForm() {
-  const textFieldStyle = { width: { xs: "80%", sm: "70%" }, mb: "25px" };
+  const textFieldStyle = { width: { xs: '80%', sm: '70%' }, mb: '25px' };
   const location = useLocation();
   const history = useHistory();
 
   const [category, setCategory] = useState(location.state.product.category);
   const [name, setName] = useState(location.state.product.productName);
-  const [description, setDescription] = useState(
-    location.state.product.description
-  );
+  const [description, setDescription] = useState(location.state.product.description);
   const [price, setPrice] = useState(location.state.product.price);
   const [coverPic, setCoverPic] = useState(location.state.product.picProduct);
   const [image, setImage] = useState(location.state.product.picProduct);
-  const [externalLink, setExternalLink] = useState(
-    location.state.product.externalLink
-  );
+  const [externalLink, setExternalLink] = useState(location.state.product.externalLink);
 
-  console.log(location.state.category);
+  const [isFocus, setIsFocus] = useState({
+    productName: false,
+    price: false,
+    description: false
+  });
 
   const [optionCategory, setOptionCategory] = useState([]);
   useEffect(() => {
     const fetchCategory = async () => {
       try {
-        const res = await axios.get("/category");
+        const res = await axios.get('/category');
         const fetChcategorys = res.data.categorys;
         setOptionCategory(fetChcategorys);
         console.log(fetChcategorys);
@@ -40,36 +40,32 @@ function EditProductForm() {
     fetchCategory();
   }, []);
 
-  const handleChangeFile = (e) => {
+  const handleChangeFile = e => {
     // console.log(e.target.files);
     setImage(URL.createObjectURL(e.target.files[0]));
     setCoverPic(e.target.files[0]);
   };
 
-  const handleEditProduct = async (e) => {
+  const handleEditProduct = async e => {
     try {
       e.preventDefault();
 
       const formData = new FormData();
-      formData.append("name", name);
-      formData.append("description", description);
-      formData.append("categoryId", category);
-      formData.append("price", price);
-      formData.append("coverPic", coverPic);
-      formData.append("externalLink", externalLink);
-      formData.append("hashtag", "link to product");
+      formData.append('name', name);
+      formData.append('description', description);
+      formData.append('categoryId', category);
+      formData.append('price', price);
+      formData.append('coverPic', coverPic);
+      formData.append('externalLink', externalLink);
+      formData.append('hashtag', 'link to product');
 
       const config = {
         headers: {
-          "content-type": "multipart/form-data",
-        },
+          'content-type': 'multipart/form-data'
+        }
       };
 
-      const res = await axios.put(
-        `/product/${location.state.product.id}`,
-        formData,
-        config
-      );
+      const res = await axios.put(`/product/${location.state.product.id}`, formData, config);
 
       // const res = await axios.put(`/product/${location.state.product.id}`, {
       //   price,
@@ -82,12 +78,12 @@ function EditProductForm() {
 
       // console.log(res.data);
       Swal.fire({
-        icon: "success",
-        title: "Successfully edited the product.",
+        icon: 'success',
+        title: 'Successfully edited the product.',
         showConfirmButton: false,
-        timer: 1500,
+        timer: 1500
       });
-      history.push("/profile");
+      history.push('/profile');
       window.location.reload();
     } catch (err) {
       console.log(err);
@@ -98,57 +94,53 @@ function EditProductForm() {
   return (
     <Box
       sx={{
-        width: "100%",
+        width: '100%',
         // height: '100vh',
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#EFF1F3",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#EFF1F3'
       }}
     >
       <Box
         sx={{
-          width: { md: "72%", sm: "100%", xs: "100%" },
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "white",
-          height: "80%",
-          marginTop: "5rem",
+          width: { md: '72%', sm: '100%', xs: '100%' },
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'white',
+          height: '80%',
+          marginTop: '5rem'
         }}
       >
         <Box
           sx={{
             // height: '6%',
             // width: '70%',
-            width: { xs: "100%", sm: "100%", md: "100%" },
-            backgroundColor: "#232836",
-            color: "white",
-            display: "flex",
-            alignItems: "center",
-            padding: "20px 50px",
+            width: { xs: '100%', sm: '100%', md: '100%' },
+            backgroundColor: '#232836',
+            color: 'white',
+            display: 'flex',
+            alignItems: 'center',
+            padding: '20px 50px'
             // mt: "50px",
           }}
         >
           Edit Item
         </Box>
-        <Box sx={{ paddingTop: "30px" }}>
+        <Box sx={{ paddingTop: '30px' }}>
           <div
             style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
             }}
           >
             <img
-              src={
-                image
-                  ? image
-                  : "https://res.cloudinary.com/duca0jbyn/image/upload/v1635160655/1478594_uop0jq.png"
-              }
+              src={image ? image : 'https://res.cloudinary.com/duca0jbyn/image/upload/v1635160655/1478594_uop0jq.png'}
               style={{
-                width: "50%",
+                width: '50%'
               }}
               alt=""
             />
@@ -159,23 +151,23 @@ function EditProductForm() {
               id="contained-button-file"
               multiple
               type="file"
-              sx={{ display: "none" }}
+              sx={{ display: 'none' }}
               onChange={handleChangeFile}
             />
             <Box
               sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
               }}
             >
               <Button
                 variant="contained"
                 component="span"
                 sx={{
-                  width: "60%",
-                  bgcolor: "gray",
-                  mt: "15px",
+                  width: '60%',
+                  bgcolor: 'gray',
+                  mt: '15px'
                 }}
               >
                 Upload Profile
@@ -186,59 +178,67 @@ function EditProductForm() {
 
         <Box
           sx={{
-            width: { xs: "90%", sm: "90%", md: "70%" },
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            backgroundColor: "white",
-            padding: "80px 0px",
-            mb: "80px",
+            width: { xs: '90%', sm: '90%', md: '70%' },
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            padding: '80px 0px',
+            mb: '80px'
           }}
           component="form"
           onSubmit={handleEditProduct}
         >
           <TextField
             sx={textFieldStyle}
+            label={isFocus.name ? 'Name' : ''}
+            onFocus={() => setIsFocus(curr => ({ ...curr, name: true }))}
+            onBlur={() => setIsFocus(curr => ({ ...curr, name: false }))}
             value={name}
-            onChange={(e) => setName(e.target.value)}
+            onChange={e => setName(e.target.value)}
           />
 
           <TextField
             id="standard-multiline-static"
             value={description}
+            label={isFocus.description ? 'Description' : ''}
+            onFocus={() => setIsFocus(curr => ({ ...curr, description: true }))}
+            onBlur={() => setIsFocus(curr => ({ ...curr, description: false }))}
             multiline
             sx={textFieldStyle}
-            onChange={(e) => setDescription(e.target.value)}
+            onChange={e => setDescription(e.target.value)}
           />
           <TextField
             id="outlined-select-currency"
             select
             sx={textFieldStyle}
             value={category}
-            onChange={(e) => setCategory(e.target.value)}
+            onChange={e => setCategory(e.target.value)}
           >
-            {optionCategory.map((item) => (
+            {optionCategory.map(item => (
               <MenuItem key={item.id} value={item.id}>
                 {item.name}
               </MenuItem>
             ))}
           </TextField>
           <TextField
+            label={isFocus.price ? 'Price' : ''}
+            onFocus={() => setIsFocus(curr => ({ ...curr, price: true }))}
+            onBlur={() => setIsFocus(curr => ({ ...curr, price: false }))}
             value={price}
             sx={textFieldStyle}
-            onChange={(e) => setPrice(e.target.value)}
+            onChange={e => setPrice(e.target.value)}
           />
           <TextField
             value={externalLink}
+            label={isFocus.externalLink ? 'External Link' : ''}
+            onFocus={() => setIsFocus(curr => ({ ...curr, externalLink: true }))}
+            onBlur={() => setIsFocus(curr => ({ ...curr, externalLink: false }))}
             sx={textFieldStyle}
-            onChange={(e) => setExternalLink(e.target.value)}
+            onChange={e => setExternalLink(e.target.value)}
           />
 
-          <Button
-            type="submit"
-            variant="gradient"
-            sx={{ color: "white", p: "10px", width: { xs: "80%", sm: "70%" } }}
-          >
+          <Button type="submit" variant="gradient" sx={{ color: 'white', p: '10px', width: { xs: '80%', sm: '70%' } }}>
             Save
           </Button>
         </Box>
