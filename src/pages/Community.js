ebf6bb66e3b38564613d922ad284398ec82c7056
nattapat.aur <nattapat.aur@gmail.com>
2021-11-04 10:30:49 +0700

    added-member-sidebar

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import SideBarL from '../components/layout/sidebar/SideBarL';
-import Grid from '@mui/material/Grid';
-import CommunityContent from '../components/layout/community/CommunityContent';
+import React from "react";
+import SideBarL from "../components/layout/sidebar/SideBarL";
+import Grid from "@mui/material/Grid";
+import CommunityContent from "../components/layout/community/CommunityContent";
 
 function Community() {
-  const hide = { display: { xs: 'none', md: 'flex' } };
-  return (
-    <>
-      <Grid container sx={{ marginBottom: '30px' }}>
-        <Grid item xs={2} sx={{ ...hide }}>
-          <SideBarL />
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={10}
-          sx={{
-            margin: { xs: 'auto', md: 'initial' },
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'flex-start',
-          }}>
-          <CommunityContent />
-        </Grid>
-      </Grid>
-    </>
-  );
+	const hide = { display: { xs: "none", md: "flex" } };
+	return (
+		<>
+			<Grid container sx={{ marginBottom: "30px" }}>
+				<Grid item xs={2} sx={{ ...hide }}>
+					<SideBarL type="community" />
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					md={10}
+					sx={{
+						margin: { xs: "auto", md: "initial" },
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "flex-start",
+					}}
+				>
+					<CommunityContent />
+				</Grid>
+			</Grid>
+		</>
+	);
 }
 
 export default Community;
